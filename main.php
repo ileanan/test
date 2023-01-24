4dd03525640d2015a42fe40730f2d18d908315fe
CHUM NOEURN <soknoeurn-srun>
2023-01-24 08:16:29 +0700

    TP-2 <add space>

--- main.php
+++ main.php
@@ -9,6 +9,7 @@
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
+    
 <form class="flex items-center">   
     <label for="voice-search" class="sr-only">Search</label>
     <div class="relative w-full">
@@ -23,7 +24,7 @@
     <button type="submit" class="inline-flex items-center py-2.5 px-3 ml-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
         <svg aria-hidden="true" class="w-5 h-5 mr-2 -ml-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Search
     </button>
-    
+
 </form>
 </body>
 </html>
